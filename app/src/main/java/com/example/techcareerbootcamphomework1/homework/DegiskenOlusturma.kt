b5e691a38d4c1aad103d80fce532348833c771e1
fahricandurucan <fahricandurucan@icloud.com>
2023-03-21 14:50:54 +0300

    homework done

--- app/src/main/java/com/example/techcareerbootcamphomework1/homework/DegiskenOlusturma.kt
+++ app/src/main/java/com/example/techcareerbootcamphomework1/homework/DegiskenOlusturma.kt
@@ -2,4 +2,50 @@ package com.example.techcareerbootcamphomework1.homework
 
 fun main() {
 
+    var sehir = "Adana"
+    var ulke = "Türkiye"
+    var telefon = "0536 123 12 12"
+    var postaKodu = "01010"
+    var email = "kullanici@gmail.com"
+    var meslek = "Bilgisayar Mühendisi"
+    var stokMiktari = 100
+    var musteriAdi = "Ali"
+    var bakiye : Double = 1907.50
+    var dogumGunu = "15 Aralık"
+    var maas : Int = 19850
+    var medeniDurum : Boolean = false
+    var urunYorum = "bir beden küçük alınız"
+    val odemeTarihi = "28.06.2023"
+    val ödeme = 2500
+    val siparisAdet : Int = 58
+    val arabaModel = "Range Rover"
+    val kitapAdi = "Hayvanlar ve İnsanlar"
+    val yayınlamaTarihi = "15.12.2022"
+    val indirim = "%35"
+    val odaSayisi = 3
+    val enlem = "15°K";
+    val boylam = "30°D";
+    val urunAdi = "Iphone 13 Pro Max"
+    val yemekFiyat : Double = 89.95
+    val marka = "Puma"
+    val muzikAdi = "Arabesk"
+    val videoSure = 15.30
+    var urunPuan = 5
+    var resimAd = "Çığlık"
+    var dosyaFormati = ".png"
+    var renk = "mavi"
+    var renkKodu = "#ffffff"
+    var telefonModel = "samsung s3 mini"
+    var ekranBoyutu = 46.5
+    var agirlik = 73.2
+    var ulusalGun = "23 Nisan 1923"
+    var tatilGunu = "pazar"
+    var rezervasyon = "08.08.2023"
+    var sokakAdi = "Sıtkı Koçman Sokak"
+    var otobusHatti = "7C"
+    var kalanDakika = 10.20
+    var takipKodu = "01245"
+    var kuponSüresi = "3 gün 5 saat"
+    var faturaAdresi = "xxxx caddesi yyyy sokak Ataşehir / Istanbul"
+
 }
